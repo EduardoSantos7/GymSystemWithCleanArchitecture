45fa71bf75ab813140a812f4b3264600293e0ff8
Eduardo Luis Santos <eduardosa@microsoft.com>
2023-10-29 19:18:53 -0700

    Complete Gym queries

---
+++ GymManagement/src/GymManagement.Application/Gyms/Queries/ListGyms/ListGymsQueryHandler.cs
@@ -0,0 +1,28 @@
+using ErrorOr;
+using GymManagement.Application.Common.Interfaces;
+using GymManagement.Domain.Gyms;
+using MediatR;
+
+namespace GymManagement.Application.Gyms.Queries.ListGyms;
+
+public class ListGymsQueryHandler : IRequestHandler<ListGymsQuery, ErrorOr<List<Gym>>>
+{
+    private readonly IGymsRepository _gymsRepository;
+    private readonly ISubscriptionsRepository _subscriptionsRepository;
+
+    public ListGymsQueryHandler(IGymsRepository gymsRepository, ISubscriptionsRepository subscriptionsRepository)
+    {
+        _gymsRepository = gymsRepository;
+        _subscriptionsRepository = subscriptionsRepository;
+    }
+
+    public async Task<ErrorOr<List<Gym>>> Handle(ListGymsQuery query, CancellationToken cancellationToken)
+    {
+        if (!await _subscriptionsRepository.ExistsAsync(query.SubscriptionId))
+        {
+            return Error.NotFound(description: "Subscription not found");
+        }
+
+        return await _gymsRepository.ListBySubscriptionIdAsync(query.SubscriptionId);
+    }
+}
